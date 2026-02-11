0acb95f2c7ea93f2075532c942896740d5dd093b
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2026-02-11 00:03:36 -0300

    docs: update README with setup instructions and fix database config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -3,12 +3,14 @@ return [
     'db' => [
         'driver' => 'Pdo',
         'dsn' => 'mysql:dbname=corps;hostname=localhost',
+        'username' => '',
+        'password' => '',
     ],
     'service_manager' => [
         'factories' => [
             'LaminasDbAdapter'
             => 'Laminas\Db\Adapter\AdapterServiceFactory',
         ],
-    ],
+        ],
 ];
 
